a0ef94c70355ce927fa5d5862bf0db2c9972cd55
Arkadiusz Żmudzin <arkus777@gmail.com>
2015-03-28 14:37:57 +0100

    Do some changes :D

--- CialaNiebieskie/CialaNiebieskie.cpp
+++ CialaNiebieskie/CialaNiebieskie.cpp
@@ -2,10 +2,22 @@
 //
 
 #include "stdafx.h"
-
+#include <iostream>
+#include "Planet.h"
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	Planet solarSystem[] = { 
+								Planet("Sun"), Planet("Mercury"), Planet("Venus"),
+								Planet("Earth"), Planet("Mars"), Planet("Jupiter"),
+								Planet("Saturn"), Planet("Uranus"), Planet("Neptune") 
+							};
+	solarSystem[3].addParameter("Volume", "205002 dm3");
+	for (int i = 0; i < solarSystem[3].getParametersCount(); i++) {
+		std::cout << solarSystem[3].getParameter(i) << std::endl;
+	}
+	char asd;
+	std::cin >> asd;
 	return 0;
 }
 
